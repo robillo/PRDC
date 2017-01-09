0e09b8d5b774f8e12310107ec4a9fc2825086ea8
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-01-09 19:53:27 +0530

    ..

--- app/src/main/java/com/appbusters/robinkamboj/prdc/Main2Activity.java
+++ app/src/main/java/com/appbusters/robinkamboj/prdc/Main2Activity.java
@@ -1,7 +1,9 @@
 package com.appbusters.robinkamboj.prdc;
 
+import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -9,5 +11,6 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
     }
 }
