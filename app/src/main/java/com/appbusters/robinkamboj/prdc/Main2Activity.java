1b08243cf87d34c240f2f77177bb1ec830dd655d
Robin Kamboj <dev.robinkamboj@gmail.com>
2017-01-09 20:39:23 +0530

    Login Screen

--- app/src/main/java/com/appbusters/robinkamboj/prdc/Main2Activity.java
+++ app/src/main/java/com/appbusters/robinkamboj/prdc/Main2Activity.java
@@ -1,6 +1,7 @@
 package com.appbusters.robinkamboj.prdc;
 
 import android.app.ActionBar;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,5 +13,13 @@ public class Main2Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
+    }
+
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.button:{
+                Intent i = new Intent(this, MainActivity.class);
+            }
+        }
     }
 }
